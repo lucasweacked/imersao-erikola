b979fab2ad0c2b5fe6f3eff1ae34c844c7c38df9
Lucas Barros <lucasweacked21@gmail.com>
2024-12-10 20:43:29 -0300

    finalizando a imersao e começando o dever de casa

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,81 @@
-import { useState } from 'react';
+import { useState } from "react";
+import { KeyboardEvent } from "react";
 
-import './App.css'
+import "./App.css";
 
 export default function App() {
   const [text, setText] = useState("");
-  const [tasks, setTasks] = useState<Array<TaskProps>>([]);
+  const [tasks, setTasks] = useState<Array<Omit<TaskProps, "setDone">>>([]);
   const [error, setError] = useState("");
 
   return (
-  <>
-  <div>
-    <input type="text" name="tarefa" id="task" value={text} onChange={(event) => setText(event.target.value)} />
-    <button onClick={() => {
-      if(text.trim() === "") {
-        setError("Por favor, preencher alguma coisa.")
-        return;
-      } else {
-        setError("");
-      }
-      
-      setTasks((arrayAntigo) =>
-      [...arrayAntigo, {text: text, done: false} ])
-      setText("");
-    }}
-    >Salvar
-    </button>
-    <div><span className="error">{error !== "" ? error : null}</span></div>
-  </div>
+    <>
+      <div>
+        <input
+          type="text"
+          name="tarefa"
+          id="task"
+          value={text}
+          onChange={(event) => setText(event.target.value)}
+        />
+        <button
+          onClick={() => {
+            if (text.trim() === "") {
+              setError("Por favor, preencher alguma coisa.");
+              return;
+            } else {
+              setError("");
+            }
 
-  <ul>
-    {tasks.map((task) => (
-      <Task {...task} />
-    ))}
-  </ul>
-  </>
+            setTasks((arrayAntigo) => [
+              ...arrayAntigo,
+              { text: text, done: false },
+            ]);
+            setText("");
+          }}
+        >
+          Salvar
+        </button>
+        <div>
+          <span className="error">{error !== "" ? error : null}</span>
+        </div>
+      </div>
+
+      <ul>
+        {tasks.map((task, index) => (
+          <Task
+            {...task}
+            setDone={(done) =>
+              setTasks((arrayAntigo) => {
+                const arrayNovo = [...arrayAntigo];
+                arrayNovo[index].done = done;
+                return arrayNovo;
+              })
+            }
+          />
+        ))}
+      </ul>
+    </>
   );
 }
 
-export type TaskProps = {text: string, done: boolean}
+type TaskProps = {
+  text: string;
+  done: boolean;
+  setDone: (done: boolean) => void;
+};
 
 function Task(props: TaskProps) {
-  return <li>
-    <input type="checkbox" name="done" readOnly={true} checked={props.done} />
-    <span>{props.text}</span>
-  </li>;
+  return (
+    <li>
+      <input
+        type="checkbox"
+        name="done"
+        readOnly={true}
+        checked={props.done}
+        onChange={(event) => props.setDone(event.target.checked)}
+      />
+      <span>{props.text}</span>
+    </li>
+  );
 }
